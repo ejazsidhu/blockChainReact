225c2d40decdab2cca8d60a94b28f01f7f73da47
ejazsidhu <ejazsidhu@gmail.com>
2019-02-09 14:48:50 +0500

    nav bar added

--- src/App.js
+++ src/App.js
@@ -5,47 +5,65 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './components/home';
 // import About from './components/about';
-import Contact from './components/contact'
+import Contact from './components/contact';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 
 
 
 class App extends Component {
-  
+
 
   componentDidMount() {
-    
+
   }
   render() {
-    
+
 
     return (
-    <div className="container">
-    <div >
-    <Router>
-        <div>
-          {/* <h2>Welcome to React Router Tutorial</h2> */}
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <div >
+          <Router>
+            <div>
+              {/* <h2>Welcome to React Router Tutorial</h2> */}
+              {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <ul className="navbar-nav mr-auto">
             <li><Link to={'/'} className="nav-link"> Search Block </Link></li>
-            {/* <li><Link to={'/save'} className="nav-link">Save Block</Link></li> */}
-            {/* <li><Link to={'/about'} className="nav-link">About</Link></li> */}
+            <li><Link to={'/save'} className="nav-link">Save Block</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
           </ul>
-          </nav>
-          <hr />
+          </nav> */}
 
-          
-          <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/save' component={Contact} />
-              {/* <Route path='/about' component={About} /> */}
-          </Switch>
+              <Navbar bg="dark" variant="dark" expand="lg">
+                <Navbar.Brand href="#home">Block Chain Demo</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                    <Nav.Link to={'/home'} >Home</Nav.Link>
+                    {/* <Nav.Link to={'/about'}>About</Nav.Link> */}
+                    {/* <Nav.Link to={'/contact'}>Contact</Nav.Link> */}
+
+                  </Nav>
+
+                </Navbar.Collapse>
+              </Navbar>
+              <hr />
+              <div className="container">
+
+
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  {/* <Route path='/save' component={Contact} /> */}
+                  {/* <Route path='/about' component={About} /> */}
+                </Switch>
+              </div>
+            </div>
+          </Router>
+
         </div>
-      </Router>
-    
-    </div>
-     
-    </div>
+
+      </div>
       // <div className="App">
       //   <header className="App-header">
       //     <img src={logo} className="App-logo" alt="logo" />
